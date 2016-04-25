cceee16dc574d655319a7f95df704fe1bf63c847
Natalia Valgepea <natalia.valgepea@gmail.com>
2016-04-25 20:54:58 +0200

    Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('default', function () {
+    return gulp.src('./sass/main.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
 });
+
